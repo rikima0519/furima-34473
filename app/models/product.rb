562dc2cb4b71480ca8889e18bbecf280f7e40d2c
rikima0519 <s.rikima@icloud.com>
2021-03-02 14:18:46 +0900

    depuroi

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ApplicationRecord
+  has_many :comments
+  belomgs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_025345) do
+ActiveRecord::Schema.define(version: 2021_03_02_034155) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
