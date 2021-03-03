76e3b1a2b001693b1ca9e1b762287cccdfc45002
rikima0519 <s.rikima@icloud.com>
2021-03-03 18:22:24 +0900

    モデル単体テストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6) + '1a'}
+    password_confirmation {password}
+    family_name           {'山田'}
+    first_name            {'太郎'}
+    family_name_kana      {'ヤマダ'}
+    first_name_kana       {'タロウ'}
+    birth_day             {2018/01/05}
+  end
+end
+
