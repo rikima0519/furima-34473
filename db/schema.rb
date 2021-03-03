226f4f60d8117aeae1215c807d98b0feb4604854
rikima0519 <s.rikima@icloud.com>
2021-03-03 11:57:19 +0900

    first_commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,13 @@
 ActiveRecord::Schema.define(version: 2021_03_02_025345) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
