dfac68860591d5f6ef36d96a6140f7c5a399bb28
rikima0519 <s.rikima@icloud.com>
2021-03-03 12:45:30 +0900

    ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create
+  end
+
+  def destroy
+    user = User.find(id: current_user.id)
+    user.destroy
+  end
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+  end
+
 end
